09765cf9dcd0a133e3440db3457bc389ecf26a09
FelipeGdM <felipegmelo.42@gmail.com>
2020-02-01 15:54:38 -0300

    Add python script to republish gazebo topics
    Begin ThunderVolt integration \o/

---
+++ scripts/vision_proxy.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+    File:
+        vision_proxy.py
+
+    Description:
+        Simple python routine to separe the information published
+    from gazebo into several topics
+"""
+
+import rospy
+from std_msgs.msg import String
+from gazebo_msgs.msg import ModelStates, ModelState
+
+MODELS = ["vss_ball",
+          "robot_0",
+          "robot_1",
+          "robot_2"]
+
+pubs = {}
+
+for model in MODELS:
+    pubs[model] = rospy.Publisher("/vision/" + model, ModelState, queue_size=2)
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + " I heard %s", data.name)
+    for i, model in enumerate(data.name):
+        if model in MODELS:
+            msg = ModelState(model_name=model,
+                             pose=data.pose[i],
+                             twist=data.twist[i])
+            pubs[model].publish(msg)
+
+
+def listener():
+
+    rospy.init_node("gzb_proxy")
+    rospy.Subscriber("/gazebo/model_states", ModelStates, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
